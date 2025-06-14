5a42b28cf5af4a2b22b204a9e4da3de7ce238950
Halenur Gurel <halenur.gurel@hotmail.com>
2025-06-14 15:12:56 +0300

    Add files via upload

---
+++ task8.sql
@@ -0,0 +1,115 @@
+create table employee (
+id serial primary key,
+full_name varchar(50),
+birthday DATE,
+email varchar(100)
+);
+
+insert into employee (full_name, birthday, email) values ('Adlai Thraves', '1906/10/24', 'athraves0@google.co.jp');
+insert into employee (full_name, birthday, email) values ('Anny Dono', '1979/05/17', 'adono1@unesco.org');
+insert into employee (full_name, birthday, email) values ('Kacy Cordingley', '1925/10/03', 'kcordingley2@list-manage.com');
+insert into employee (full_name, birthday, email) values ('Evangelin Talbot', '1920/01/20', 'etalbot3@yellowbook.com');
+insert into employee (full_name, birthday, email) values ('Eloise Dunstan', '1980/12/25', 'edunstan4@goo.ne.jp');
+insert into employee (full_name, birthday, email) values ('Ava Eager', '1915/05/16', 'aeager5@ycombinator.com');
+insert into employee (full_name, birthday, email) values ('Ezechiel O''Shaughnessy', '1979/03/09', 'eoshaughnessy6@ask.com');
+insert into employee (full_name, birthday, email) values ('Dasie MacMearty', '1975/09/23', 'dmacmearty7@shinystat.com');
+insert into employee (full_name, birthday, email) values ('Martino Filipov', '1989/09/13', 'mfilipov8@a8.net');
+insert into employee (full_name, birthday, email) values ('Alleyn Denizet', '1916/12/08', 'adenizet9@scientificamerican.com');
+insert into employee (full_name, birthday, email) values ('Toinette Norfolk', '1903/03/16', 'tnorfolka@vimeo.com');
+insert into employee (full_name, birthday, email) values ('Garv Elletson', '1951/02/11', 'gelletsonb@nationalgeographic.com');
+insert into employee (full_name, birthday, email) values ('Hanson Loudian', '1971/10/27', 'hloudianc@opera.com');
+insert into employee (full_name, birthday, email) values ('Reeta Aucott', '1903/09/24', 'raucottd@usnews.com');
+insert into employee (full_name, birthday, email) values ('Evanne Chaise', '1904/07/07', 'echaisee@ovh.net');
+insert into employee (full_name, birthday, email) values ('Robbert Creed', '1985/05/21', 'rcreedf@cmu.edu');
+insert into employee (full_name, birthday, email) values ('Delbert Carden', '1952/04/18', 'dcardeng@usda.gov');
+insert into employee (full_name, birthday, email) values ('Kev Zellmer', '1940/02/16', 'kzellmerh@rambler.ru');
+insert into employee (full_name, birthday, email) values ('Oliver Briton', '1939/11/26', 'obritoni@canalblog.com');
+insert into employee (full_name, birthday, email) values ('Kacie Pendergast', '1994/11/16', 'kpendergastj@networksolutions.com');
+insert into employee (full_name, birthday, email) values ('Shanie Caldera', '1994/12/03', 'scalderak@blogspot.com');
+insert into employee (full_name, birthday, email) values ('Paxon Bamsey', '1930/12/04', 'pbamseyl@irs.gov');
+insert into employee (full_name, birthday, email) values ('Modestine Brangan', '1955/10/28', 'mbranganm@lulu.com');
+insert into employee (full_name, birthday, email) values ('Morganica Windham', '1929/06/03', 'mwindhamn@businessweek.com');
+insert into employee (full_name, birthday, email) values ('Anissa Simonnet', '1998/09/05', 'asimonneto@ning.com');
+insert into employee (full_name, birthday, email) values ('Rowney Costan', '1958/06/23', 'rcostanp@1688.com');
+insert into employee (full_name, birthday, email) values ('Butch Constant', '1996/05/23', 'bconstantq@twitpic.com');
+insert into employee (full_name, birthday, email) values ('Dorian Stanyland', '1991/08/06', 'dstanylandr@princeton.edu');
+insert into employee (full_name, birthday, email) values ('Ethelyn MacTrusty', '1905/07/08', 'emactrustys@discovery.com');
+insert into employee (full_name, birthday, email) values ('Chas Vittori', '1998/06/07', 'cvittorit@devhub.com');
+insert into employee (full_name, birthday, email) values ('Doti Duggleby', '1939/07/23', 'ddugglebyu@nih.gov');
+insert into employee (full_name, birthday, email) values ('Regen Cunnah', '1911/06/22', 'rcunnahv@e-recht24.de');
+insert into employee (full_name, birthday, email) values ('Roanna Aylin', '1906/10/25', 'raylinw@macromedia.com');
+insert into employee (full_name, birthday, email) values ('Briant Cluff', '1941/06/25', 'bcluffx@spiegel.de');
+insert into employee (full_name, birthday, email) values ('Franzen Wallwork', '1909/10/15', 'fwallworky@flickr.com');
+insert into employee (full_name, birthday, email) values ('Tate Towson', '1985/05/13', 'ttowsonz@virginia.edu');
+insert into employee (full_name, birthday, email) values ('Celinda Bedow', '1919/09/16', 'cbedow10@ucsd.edu');
+insert into employee (full_name, birthday, email) values ('Guthrie Surmon', '1958/05/08', 'gsurmon11@mediafire.com');
+insert into employee (full_name, birthday, email) values ('Wildon Hazeldean', '1952/04/30', 'whazeldean12@sbwire.com');
+insert into employee (full_name, birthday, email) values ('Sabra Delahunt', '1932/07/06', 'sdelahunt13@blinklist.com');
+insert into employee (full_name, birthday, email) values ('Tally Debenham', '1919/04/22', 'tdebenham14@i2i.jp');
+insert into employee (full_name, birthday, email) values ('Arny Pringle', '1919/07/29', 'apringle15@economist.com');
+insert into employee (full_name, birthday, email) values ('Joela Doggart', '1915/07/01', 'jdoggart16@163.com');
+insert into employee (full_name, birthday, email) values ('Florencia Sapsed', '1994/12/07', 'fsapsed17@nymag.com');
+insert into employee (full_name, birthday, email) values ('Karilynn Larwell', '1908/12/02', 'klarwell18@google.de');
+insert into employee (full_name, birthday, email) values ('Sherwood Shillabeer', '1928/11/17', 'sshillabeer19@flavors.me');
+insert into employee (full_name, birthday, email) values ('Quintus Simpson', '1973/06/01', 'qsimpson1a@sfgate.com');
+insert into employee (full_name, birthday, email) values ('Nathalie Scaplehorn', '1940/03/28', 'nscaplehorn1b@fotki.com');
+insert into employee (full_name, birthday, email) values ('Erin Vallery', '1954/08/22', 'evallery1c@people.com.cn');
+insert into employee (full_name, birthday, email) values ('Darill Philippet', '1993/02/21', 'dphilippet1d@tripadvisor.com');
+
+--1. update id'si 1 olan veriyi aşağıdaki verilerle değiştirdim.
+update employee
+set full_name = 'Halenur Gürel',
+	birthday = '1995-08-17',
+	email = 'halenur.gurel@hotmail.com'
+where id = 1
+returning *;
+
+--2. update Adı Anny Dono olan verinin birthday sütunundaki veriyi değiştirdim.
+update employee
+	set birthday = '1990-02-15'
+where full_name = 'Anny Dono'
+returning *;
+
+--3. update Adı A ile başlayanların adını Null olarak değiştirdik.
+update employee
+	set full_name = 'Null'
+where full_name like 'A%'
+returning *;
+
+--4. update adının sonu y ile bitenlerin e-mail adresini null olarak değiştirdik.
+update employee
+	set email ='Null'
+	where full_name like '%y'
+	returning *;
+
+--5. update email'inin sonu 
+update employee
+set email = 'null'
+where email like '%.de'
+returning *;
+
+--1. delete işlemi
+delete from employee
+where full_name ='Eloise Dunstan'
+returning *;
+
+--2. delete işlemi
+delete from employee
+where id = 16
+returning *;
+
+--3. delete işlemi
+delete from employee
+where email = 'cbedow10@ucsd.edu'
+returning *;
+
+--4. delete işlemi
+delete from employee
+where email = 'null'
+returning *;
+
+--5. delete işlemi
+delete from employee
+where full_name like '_ull'
+returning *;
+
