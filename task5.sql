2aeb8752155174fe2a3e67f75cef1b861e9e20a2
Halenur Gurel <halenur.gurel@hotmail.com>
2025-06-15 19:58:18 +0300

    Update task5.sql

--- task5.sql
+++ task5.sql
@@ -14,7 +14,7 @@ limit 5;
 --customer tablosunda bulunan last_name sütununa göre azalan yapılan sıralamada store_id 1 olmak koşuluyla ilk 4 veriyi sıralayınız.
 select last_name, store_id from customer
 where store_id = 1
-order by last_name
+order by last_name desc
 limit 4;
 
 --
